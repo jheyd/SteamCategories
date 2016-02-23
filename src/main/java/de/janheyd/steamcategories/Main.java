f8cf98b43062ad3ed7256ba3eb2a3478482ef8f6
Jan Heyd <j.heyd@mailbox.tu-berlin.de>
2016-02-23 21:06:47 +0100

    extracted json parsing to new class SteamConfig

--- src/main/java/de/janheyd/steamcategories/Main.java
+++ src/main/java/de/janheyd/steamcategories/Main.java
@@ -1,6 +1,5 @@
 package de.janheyd.steamcategories;
 
-import static java.util.stream.Collectors.toList;
 import static javax.swing.JOptionPane.showMessageDialog;
 import static javax.swing.UIManager.getSystemLookAndFeelClassName;
 import static org.apache.commons.io.FileUtils.readFileToString;
@@ -8,16 +7,12 @@ import static org.apache.commons.io.FileUtils.readFileToString;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map.Entry;
-import java.util.stream.StreamSupport;
 
 import javax.swing.JFileChooser;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main {
 
@@ -27,11 +22,10 @@ public class Main {
 			File file;
 			if (args.length >= 1)
 				file = new File(args[0]);
-			else {
+			else
 				file = load();
-				if (file == null)
-					return;
-			}
+			if (file == null)
+				return;
 
 			String vdf = loadVdf(file);
 			String json = vdfToJson(vdf);
@@ -44,29 +38,11 @@ public class Main {
 	}
 
 	private static List<Game> getGames(String json) {
-		ObjectMapper mapper = new ObjectMapper();
-
-		JsonNode apps;
 		try {
-			apps = mapper.readTree(json).get("UserRoamingConfigStore")
-				.get("Software").get("Valve").get("Steam").get("apps");
+			return SteamConfig.fromJson(json).getGameConfigs();
 		} catch (JsonProcessingException e) {
 			throw new RuntimeException("invalid JSON", e);
-		} catch (IOException e) {
-			throw new RuntimeException("unexpected IOException during String processing", e);
 		}
-		Iterable<Entry<String, JsonNode>> iterable = () -> apps.fields();
-		return StreamSupport.stream(iterable.spliterator(), false)
-			.map(entry -> {
-				String key = entry.getKey();
-				JsonNode value = entry.getValue();
-				try {
-					JsonGame jsonGame = mapper.treeToValue(value, JsonGame.class);
-					return jsonGame.toGame(key);
-				} catch (JsonProcessingException e) {
-					throw new RuntimeException(e);
-				}
-			}).collect(toList());
 	}
 
 	private static File load() {
---
+++ src/main/java/de/janheyd/steamcategories/SteamConfig.java
@@ -0,0 +1,51 @@
+package de.janheyd.steamcategories;
+
+import static java.util.stream.Collectors.toList;
+import static lombok.AccessLevel.PROTECTED;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.stream.StreamSupport;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor(access = PROTECTED)
+public class SteamConfig {
+
+	private static ObjectMapper mapper = new ObjectMapper();
+
+	private JsonNode jsonConfig;
+
+	public static SteamConfig fromJson(String json) throws JsonProcessingException {
+		try {
+			return new SteamConfig(mapper.readTree(json));
+		} catch (JsonProcessingException e) {
+			throw e;
+		} catch (IOException e) {
+			throw new RuntimeException("unexpected IOException during String processing", e);
+		}
+	}
+
+	public List<Game> getGameConfigs() {
+		JsonNode apps = jsonConfig.get("UserRoamingConfigStore")
+			.get("Software").get("Valve").get("Steam").get("apps");
+		Iterable<Entry<String, JsonNode>> iterable = () -> apps.fields();
+		return StreamSupport.stream(iterable.spliterator(), false)
+			.map(entry -> {
+				String key = entry.getKey();
+				JsonNode value = entry.getValue();
+				try {
+					JsonGame jsonGame = mapper.treeToValue(value, JsonGame.class);
+					return jsonGame.toGame(key);
+				} catch (JsonProcessingException e) {
+					throw new RuntimeException(e);
+				}
+			}).collect(toList());
+	}
+
+}
