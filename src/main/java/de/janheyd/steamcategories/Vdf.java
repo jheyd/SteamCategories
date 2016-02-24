72ddc89d3d587b7a69fc78b043dac7fd690929f7
Jan Heyd <j.heyd@mailbox.tu-berlin.de>
2016-02-24 02:05:31 +0100

    integrated simple field conversion into complex field conversion methods

--- src/main/java/de/janheyd/steamcategories/Vdf.java
+++ src/main/java/de/janheyd/steamcategories/Vdf.java
@@ -9,23 +9,18 @@ public class Vdf {
 
 	public String toJson() {
 		String s = content;
-		s = convertDictionaryKeysForComplexFields(s);
-		s = convertSimpleFields(s);
-		s = addCommasAfterComplexFields(s);
+		s = convertDictionaryKeys(s);
+		s = addCommas(s);
 		s = removeUnnecessaryCommas(s);
 		return "{" + s + "}";
 	}
 
-	private String addCommasAfterComplexFields(String s) {
-		return s.replaceAll("\\}(\\s*\")", "},$1");
+	private String addCommas(String s) {
+		return s.replaceAll("([\\}\"])(\\s*\")", "$1,$2");
 	}
 
-	private String convertDictionaryKeysForComplexFields(String s) {
-		return s.replaceAll("\"([^\"]*)\"(\\s*)\\{", "\"$1\" : \\{");
-	}
-
-	private String convertSimpleFields(String s) {
-		return s.replaceAll("\"([^\"]*)\"\\s*\"([^\"]*)\"", "\"$1\": \"$2\",");
+	private String convertDictionaryKeys(String s) {
+		return s.replaceAll("\"([^\"]*)\"\\s*([\\{\"])", "\"$1\" : $2");
 	}
 
 	private String removeUnnecessaryCommas(String s) {
