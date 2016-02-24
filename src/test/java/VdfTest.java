223e34d286928a1a451ab3ea67c5f4ed3ac5c824
Jan Heyd <j.heyd@mailbox.tu-berlin.de>
2016-02-24 01:57:59 +0100

    added junit test for vdf to json conversion

---
+++ src/test/java/VdfTest.java
@@ -0,0 +1,23 @@
+import static net.javacrumbs.jsonunit.JsonMatchers.jsonEquals;
+import static org.apache.commons.io.FileUtils.readFileToString;
+import static org.junit.Assert.assertThat;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import de.janheyd.steamcategories.Vdf;
+
+public class VdfTest {
+
+	@Test
+	public void testToJson() throws Exception {
+		String input = readFileToString(new File("src/test/resources/sharedconfig.vdf"));
+		String expectedOutput = readFileToString(new File("src/test/resources/expected"));
+
+		String acutalOutput = new Vdf(input).toJson();
+
+		assertThat(acutalOutput, jsonEquals(expectedOutput));
+	}
+
+}
