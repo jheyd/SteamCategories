9f6063964dc13e249a8e5d247150c2dc7e4f809d
Jan Heyd <j.heyd@mailbox.tu-berlin.de>
2016-02-29 12:04:18 +0100

    implemented json to vdf conversion

--- src/main/java/de/janheyd/steamcategories/Vdf.java
+++ src/main/java/de/janheyd/steamcategories/Vdf.java
@@ -4,6 +4,8 @@ import static org.apache.commons.io.FileUtils.readFileToString;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import lombok.AllArgsConstructor;
 
@@ -16,11 +18,65 @@ public class Vdf {
 		return new Vdf(readFileToString(file));
 	}
 
+	public static Vdf fromJson(String json) {
+		String s = json;
+		s = removeBrackets(s);
+		s = convertDictionaryKeysToVdf(s);
+		s = removeCommas(s);
+		return new Vdf(s);
+	}
+
+	private static String convertDictionaryKeysForSimpleFieldsToVdf(String s) {
+		return s.replaceAll("\"([^\"]*)\"\\s*\\:\\s*([\"])", "\"$1\"\t\t$2");
+	}
+
+	private static String convertDictionaryKeysToVdf(String s) {
+		String result = s;
+		result = convertDictionaryKeysForSimpleFieldsToVdf(s);
+		result = result.replaceAll("\"([^\"]*)\"\\s*\\:\\s*([\\{])", "\"$1\"\n$2");
+		List<String> modifiedLines = new ArrayList<>();
+		int previousIndent = 0;
+		for (String line : result.split("\r?\n")) {
+			String modifiedLine = line;
+			if (line.startsWith("{"))
+				for (int i = 0; i < previousIndent; i++ )
+					modifiedLine = "\t" + modifiedLine;
+			else
+				previousIndent = countLeadingTabs(line);
+			modifiedLines.add(modifiedLine);
+		}
+		result = String.join("\n", modifiedLines) + "\n";
+		return result;
+	}
+
+	private static int countLeadingTabs(String line) {
+		int result = 0;
+		for (char c : line.toCharArray())
+			if (c == '\t')
+				result++ ;
+			else
+				break;
+		return result;
+	}
+
+	private static String removeBrackets(String s) {
+		return s.substring(1, s.length() - 1);
+	}
+
+	private static String removeCommas(String s) {
+		return s.replaceAll("([\\}\"]),", "$1");
+	}
+
 	public String toJson() {
 		String s = content;
 		s = convertDictionaryKeys(s);
 		s = addCommas(s);
 		return "{" + s + "}";
+	}
+
+	@Override
+	public String toString() {
+		return content;
 	}
 
 	private String addCommas(String s) {
--- src/test/java/de/janheyd/steamcategories/VdfTest.java
+++ src/test/java/de/janheyd/steamcategories/VdfTest.java
@@ -2,6 +2,7 @@ package de.janheyd.steamcategories;
 
 import static net.javacrumbs.jsonunit.JsonMatchers.jsonEquals;
 import static org.apache.commons.io.FileUtils.readFileToString;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.io.File;
@@ -9,6 +10,16 @@ import java.io.File;
 import org.junit.Test;
 
 public class VdfTest {
+
+	@Test
+	public void testFromJson() throws Exception {
+		String input = readFileToString(new File("src/test/resources/sharedconfig.json"));
+		String expectedOutput = readFileToString(new File("src/test/resources/sharedconfig.vdf"));
+
+		String actualOutput = Vdf.fromJson(input).toString();
+
+		assertThat(actualOutput, is(expectedOutput));
+	}
 
 	@Test
 	public void testToJson() throws Exception {
